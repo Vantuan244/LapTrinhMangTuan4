5e13a584ef086f2790a9d691b1e4ab28905dbd71
Pigeonchill <94815802+Pigeonchill@users.noreply.github.com>
2024-01-29 21:18:38 +0700

    đasd
    ádasđá

---
+++ JavaApplication2/src/javaapplication2/tuan4/Bai456.java
@@ -0,0 +1,34 @@
+package javaapplication2.tuan4;
+import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+public class Bai456 {
+    public static void main(String[] args) {
+        try {
+            URL url = new URL("https://sharecode.vn/tim-kiem.htm?tu-khoa=code&ngon-ngu=0");
+            //Phan tich thong tin tu url
+            System.out.println("Protocol : "+url.getProtocol());
+            System.out.println("Host : "+url.getHost());
+            System.out.println("Port : " + url.getPort());
+            System.out.println("Path : " + url.getPath());
+            System.out.println("Query : " + url.getQuery());
+            System.out.println("Reference : " + url.getRef());
+            URLConnection urlConnection = url.openConnection();
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()))) {
+                String line;
+                FileWriter fileWriter = new FileWriter("data.html");
+                
+                while((line = reader.readLine()) != null){
+                    System.out.println(line);
+                    fileWriter.write(line + "\n");
+                }
+                fileWriter.close();
+            }
+            
+        } catch (IOException e) {
+        }
+    }
+}
